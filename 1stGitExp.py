2cdbc181edca2b83bd982c87d13d46aeb981d1fd
iamkhaidarzakirov <zakirovdiy@gmail.com>
2022-07-17 18:23:58 +0300

    firstCommit

---
+++ 1stGitExp.py
@@ -0,0 +1,11 @@
+def foo(bar):
+    for i in range(1001):
+        print(bar)
+    return bar
+
+
+bar = 'Fucked Up Beyond All Recognition'
+
+foo(bar)
+
+# It's just one example code to my firsrt upload in Git.
